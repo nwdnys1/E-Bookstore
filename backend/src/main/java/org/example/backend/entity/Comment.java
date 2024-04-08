d86b2962274378f27353186815860673bc722078
nwdnys1 <zh13588083809@sjtu.edu.cn>
2024-04-09 00:24:32 +0800

    comment

---
+++ backend/src/main/java/org/example/backend/entity/Comment.java
@@ -0,0 +1,30 @@
+package org.example.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "comments")
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","book"})//忽略cartItems属性 并且解决cartItems属性为null的问题
+public class Comment {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private int id;
+    private String content;
+    private LocalDateTime time;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uid")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bid")
+    private Book book;
+}
---
+++ backend/src/main/java/org/example/backend/serviceImpl/CommentServiceImpl.java
@@ -0,0 +1,4 @@
+package org.example.backend.serviceImpl;
+
+public class CommentServiceImpl {
+}
